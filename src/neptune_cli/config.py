95ed3536d6dc2b24d102995cad29b4b00cfecae3
Mark S <mark@shuttle.dev>
2025-12-01 11:35:53 -0500

    refactor: implement `PydanticBaseSettingsSource` loader for JSON file
    Signed-off-by: Mark S <mark@shuttle.dev>

--- src/neptune_cli/config.py
+++ src/neptune_cli/config.py
@@ -1,26 +1,91 @@
+# Standard Library Imports
+import json
+import os
+from functools import cached_property
 from pathlib import Path
+from typing import Any
 
+# Third-Party Imports
 from platformdirs import user_config_dir
+from pydantic import (
+    Field,
+    computed_field,
+)
+from pydantic.fields import FieldInfo
 from pydantic_settings import (
     BaseSettings,
-    JsonConfigSettingsSource,
     PydanticBaseSettingsSource,
     SettingsConfigDict,
 )
+
+LOCAL_API_BASE_URL: str = "http://localhost:8000/v1"
+DEFAULT_API_BASE_URL: str = "https://neptune.shuttle.dev/v1"
+
+NEPTUNE_CFG_FILE_PATH: Path = Path(user_config_dir("neptune")) / "config.json"
+
+os.environ["NEPTUNE_API_BASE_URL"] = (
+    LOCAL_API_BASE_URL
+    if str(os.environ.get("NEPTUNE_API_ENV")).casefold() == "local"
+    else DEFAULT_API_BASE_URL
+)
+
+
+class NeptuneJsonConfigSource(PydanticBaseSettingsSource):
+    """A simple settings source class that loads variables from a JSON file
+    in the local platform's configuration directory.
+    """
+
+    @computed_field
+    @cached_property
+    def _json_data(self) -> dict[str, Any]:
+        data = json.loads(NEPTUNE_CFG_FILE_PATH.read_bytes())
+
+        return data
+
+    def get_field_value(
+        self,
+        field: FieldInfo,
+        field_name: str,
+    ) -> tuple[Any, str, bool]:
+        value = self._json_data.get(field_name) or field.default_factory()
+
+        return value, field_name, False
+
+    def __call__(self) -> dict[str, Any]:
+        data: dict[str, Any] = {}
+
+        for field_name, field in self.settings_cls.model_fields.items():
+            field_value, field_key, value_is_complex = self.get_field_value(
+                field,
+                field_name,
+            )
+
+            data[field_key] = self.prepare_field_value(
+                field_name,
+                field,
+                field_value,
+                value_is_complex,
+            )
+
+        return data
 
 
 class CLISettings(BaseSettings):
     """Configuration settings for the Neptune CLI."""
 
-    # Local development: use `NEPTUNE_API_BASE_URL=http://localhost:8000/v1`
-    api_base_url: str = "https://neptune.shuttle.dev/v1"
-
-    access_token: str | None = None
-
     model_config = SettingsConfigDict(
         env_prefix="NEPTUNE_",
-        json_file=Path(user_config_dir("neptune")) / "config.json",
     )
+
+    access_token: str | None = Field(default=None)
+    api_base_url: str = Field(default=DEFAULT_API_BASE_URL)
+
+    def save_to_file(self) -> None:
+        """Save the current settings to the configuration file."""
+        NEPTUNE_CFG_FILE_PATH.parent.mkdir(parents=True, exist_ok=True)
+
+        with NEPTUNE_CFG_FILE_PATH.open("w") as writer:
+            writer.write(self.model_dump_json())
 
     @classmethod
     def settings_customise_sources(
@@ -36,15 +101,8 @@ class CLISettings(BaseSettings):
             env_settings,
             dotenv_settings,
             file_secret_settings,
-            JsonConfigSettingsSource(settings_cls),
+            NeptuneJsonConfigSource(settings_cls),
         )
-
-    def save_to_file(self) -> None:
-        """Save the current settings to the configuration file."""
-        config_path = Path(user_config_dir("neptune"))
-        config_path.mkdir(parents=True, exist_ok=True)
-        with open(config_path / "config.json", "w") as f:
-            f.write(self.model_dump_json())
 
 
 SETTINGS = CLISettings()
