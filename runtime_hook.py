17df2ab95b5b4f98b876652db235b6d12bf9fc0a
Ivan Pedrazas <ipedrazas@gmail.com>
2025-12-19 11:32:33 +0000

    chore: undo ruff import cleanup
    Signed-off-by: Ivan Pedrazas <ipedrazas@gmail.com>

--- runtime_hook.py
+++ runtime_hook.py
@@ -1,1 +1,9 @@
+# ruff: noqa
 # Runtime hook to ensure stdlib modules are available
+import pickletools
+import pickle
+import sqlite3
+import webbrowser
+import difflib
+import stringprep
+import encodings.idna
